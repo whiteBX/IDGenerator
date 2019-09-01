136f6c182dcba830fd4e389beb6b5651812175f7
white <baixiong2003@gmail.com>
2019-09-01 15:38:27 +0800

    update desc

--- service/src/main/java/org/white/idgenerator/service/entity/IdGenerateInfo.java
+++ service/src/main/java/org/white/idgenerator/service/entity/IdGenerateInfo.java
@@ -4,7 +4,7 @@ package org.white.idgenerator.service.entity;
  * <p>
  * 总构成：
  * 版本号2位 + 秒级时间30位 + 序列号20位 + 机器id5位 = 57位二进制
- * 除时间30位外需要扩充的可以扩充,但是位置必须跟着改
+ * 需要扩充的可以扩充,但是位置必须跟着改
  * </p >
  *
  * @author white
